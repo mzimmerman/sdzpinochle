07b1d6dd73729d550d6da7b29ef13f06b6f3e516
Matthew Zimmerman <mzimmerman@gmail.com>
2013-01-07 15:40:16 -0500

    added a Meld function to Hand and a bunch of tests

--- pinochle.go
+++ pinochle.go
@@ -1,35 +1,42 @@
 // pinochle.go
-package main
+package sdzpinochle
 
 import (
 	"fmt"
 	"math/rand"
+	"sort"
 	"time"
 )
 
 const (
-	nine = iota
-	jack
-	queen
-	king
+	ace = iota
 	ten
-	ace
+	king
+	queen
+	jack
+	nine
 )
 
 const (
 	spades = iota
 	hearts
-	diamonds
 	clubs
+	diamonds
+)
+
+const (
+	acearound   = 10
+	kingaround  = 8
+	queenaround = 6
+	jackaround  = 4
 )
 
 type Card struct {
-	card int
-	suit int
+	card, suit int
 }
 
 type Deck [48]Card
-type Hand [12]Card
+type Hand []Card
 
 func (c Card) String() string {
 	var str string
@@ -65,9 +72,7 @@ func (c Card) isTrump(trump int) bool {
 }
 
 func (d *Deck) Swap(i, j int) {
-	victim := d[i]
-	d[i] = d[j]
-	d[j] = victim
+	d[i], d[j] = d[j], d[i]
 }
 
 func (d *Deck) Shuffle() {
@@ -80,11 +85,36 @@ func (d *Deck) Shuffle() {
 	}
 }
 
-func (d Deck) Deal() (hands [4]Hand) {
+func (h Hand) String() {
+	cards := ""
+	for x := 0; x < len(h); x++ {
+		cards += h[x].String() + " "
+	}
+}
+
+func (h Hand) Len() int {
+	return len(h)
+}
+
+func (h Hand) Less(i, j int) bool {
+	if h[i].suit == h[j].suit {
+		return h[i].card < h[j].card
+	}
+	return h[i].suit < h[j].suit
+}
+
+func (h Hand) Swap(i, j int) {
+	h[i], h[j] = h[j], h[i]
+}
+
+func (d Deck) Deal() (hands []Hand) {
+	hands = make([]Hand, 4)
+	for x := 0; x < 4; x++ {
+		hands[x] = make([]Card, 12)
+	}
 	for y := 0; y < 12; y++ {
 		for x := 0; x < 4; x++ {
-			hands[x][y] = d[y+x]
-			fmt.Println("hands[%v][%v] = deck[%v]", x, y, (y*4)+x)
+			hands[x][y] = d[y*4+x]
 		}
 	}
 	return
@@ -97,7 +127,7 @@ func createDeck() (deck Deck) {
 	for x := 0; x < len(cards); x++ {
 		for y := 0; y < len(suits); y++ {
 			for z := 0; z < 2; z++ {
-				deck[index] = Card{cards[x], suits[y]}
+				deck[index] = Card{card: cards[x], suit: suits[y]}
 				index++
 			}
 		}
@@ -105,19 +135,150 @@ func createDeck() (deck Deck) {
 	return
 }
 
-func main() {
-	fmt.Println("Hello World!")
-	deck := createDeck()
-	deck.Shuffle()
-	for x := 0; x < len(deck); x++ {
-		fmt.Println(deck[x].String())
-	}
-	fmt.Println("----")
-	hands := deck.Deal()
+type Player struct {
+	hand Hand
+}
+
+type AI struct {
+	Player
+}
+
+type Game struct {
+	deck Deck
+	p    []Player
+}
+
+func createGame() (game *Game) {
+	game.deck = createDeck()
+	game.deck.Shuffle()
+	hands := game.deck.Deal()
 	for x := 0; x < 4; x++ {
-		fmt.Println("Hand %d", x+1)
-		for y := 0; y < len(hands[x]); y++ {
-			fmt.Println(hands[x][y].String())
+		game.p[x].hand = hands[x]
+		sort.Sort(game.p[x].hand)
+	}
+	return
+}
+
+func (h Hand) Count() (cards map[Card]int) {
+	cards = make(map[Card]int)
+	//	for card := range []int{ace, ten, king, queen, jack, nine} {
+	//		for suit := range []int{spades, hearts, clubs, diamonds} {
+	//			cards[Card{suit:suit, card:card}] = 0
+	//		}
+	//	}
+	for x := 0; x < len(h); x++ {
+		cards[h[x]]++
+	}
+	return
+}
+
+func min(a, b int) int {
+	if a < b {
+		return a
+	}
+	return b
+}
+
+func (h Hand) Meld(trump int) (meld int) {
+	return h.MeldDebug(trump, false)
+}
+
+func (h Hand) MeldDebug(trump int, debug bool) (meld int) {
+	// hand does not have to be sorted
+	count := h.Count()
+	around := [6]int{}
+	for _, value := range []int{ace, king, queen, jack} {
+		around[value] = 2
+	}
+	//	fmt.Printf("ace %d ten %d king %d queen %d jack %d nine %d\n", ace, ten, king, queen, jack, nine)
+	//	fmt.Printf("AroundBefore = %v\n", around)
+	for _, suit := range []int{spades, hearts, clubs, diamonds} { // look through each suit
+		switch { // straights & marriages
+		case trump == suit:
+			if debug {
+				fmt.Printf("Scoring %d nine(s) in trump %d\n", count[Card{suit: suit, card: nine}], suit)
+			}
+			meld += count[Card{suit: suit, card: nine}] // 9s in trump
+			switch {
+			// double straight
+			case count[Card{suit: suit, card: ace}] == 2 && count[Card{suit: suit, card: ten}] == 2 && count[Card{suit: suit, card: king}] == 2 && count[Card{suit: suit, card: queen}] == 2 && count[Card{suit: suit, card: jack}] == 2:
+				meld += 150
+				if debug {
+					fmt.Println("DoubleStraight")
+				}
+			// single straight
+			case count[Card{suit: suit, card: ace}] >= 1 && count[Card{suit: suit, card: ten}] >= 1 && count[Card{suit: suit, card: king}] >= 1 && count[Card{suit: suit, card: queen}] >= 1 && count[Card{suit: suit, card: jack}] >= 1:
+				if count[Card{suit: suit, card: king}] == 2 && count[Card{suit: suit, card: queen}] == 2 {
+					meld += 19
+					if debug {
+						fmt.Println("SingleStraightWithExtraMarriage")
+					}
+				} else {
+					if debug {
+						fmt.Println("SingleStraight")
+					}
+					meld += 15
+				}
+			case count[Card{suit: suit, card: king}] == 2 && count[Card{suit: suit, card: queen}] == 2:
+				meld += 8
+				if debug {
+					fmt.Println("DoubleMarriageInTrump")
+				}
+			case count[Card{suit: suit, card: king}] >= 1 && count[Card{suit: suit, card: queen}] >= 1:
+				meld += 4
+				if debug {
+					fmt.Println("SingleMarriageInTrump")
+				}
+			}
+		case count[Card{suit: suit, card: king}] == 2 && count[Card{suit: suit, card: queen}] == 2:
+			meld += 4
+			if debug {
+				fmt.Println("DoubleMarriage")
+			}
+		case count[Card{suit: suit, card: king}] >= 1 && count[Card{suit: suit, card: queen}] >= 1:
+			if debug {
+				fmt.Println("SingleMarriage")
+			}
+			meld += 2
+		}
+		for _, value := range [4]int{ace, king, queen, jack} { // looking for "around" meld
+			//						fmt.Printf("Looking for %d in suit %d\n", value, suit)
+			around[value] = min(count[Card{suit: suit, card: value}], around[value])
 		}
 	}
+	for _, value := range [4]int{ace, king, queen, jack} {
+		if around[value] > 0 {
+			var worth int
+			switch value {
+			case ace:
+				worth = acearound
+			case king:
+				worth = kingaround
+			case queen:
+				worth = queenaround
+			case jack:
+				worth = jackaround
+			}
+			if around[value] == 2 {
+				worth *= 10
+			}
+			meld += worth
+			if debug {
+				fmt.Printf("Around-%d\n", worth)
+			}
+		}
+	}
+	switch { // pinochle
+	case count[Card{suit: diamonds, card: jack}] == 2 && count[Card{suit: spades, card: queen}] == 2:
+		meld += 30
+		if debug {
+			fmt.Println("DoubleNochle")
+		}
+	case count[Card{suit: diamonds, card: jack}] >= 1 && count[Card{suit: spades, card: queen}] >= 1:
+		meld += 4
+		if debug {
+			fmt.Println("Nochle")
+		}
+	}
+	return
 }
--- pinochle_test.go
+++ pinochle_test.go
@@ -28,23 +28,175 @@ func fakeDeal(d *Deck) (h []Hand) {
 	return
 }
 
-func TestHands(t *testing.T) {
+func TestDeal(t *testing.T) {
 	deck := createDeck()
 	var h []Hand
 	h = fakeDeal(&deck)
-	fmt.Println("Deck Created")
+	//	fmt.Println("Deck Created")
 	checkForDupes(h, t)
 	deck.Shuffle()
-	fmt.Println("Deck Shuffled")
+	//	fmt.Println("Deck Shuffled")
 	h = fakeDeal(&deck)
 	checkForDupes(h, t)
 	h = deck.Deal()
-	fmt.Println("Deck Dealt")
+	//	fmt.Println("Deck Dealt")
 	checkForDupes(h, t)
 	sort.Sort(h[0])
 	sort.Sort(h[1])
 	sort.Sort(h[2])
 	sort.Sort(h[3])
-	fmt.Println("Hands Sorted")
+	//	fmt.Println("Hands Sorted")
 	checkForDupes(h, t)
+}
+
+func TestMin(t *testing.T) {
+	if min(1, 2) != 1 {
+		t.FailNow()
+	}
+	if min(2, 1) != 1 {
+		t.FailNow()
+	}
+	if min(5, 5) != 5 {
+		t.FailNow()
+	}
+}
+
+func TestCount(t *testing.T) {
+	hand := Hand{
+		Card{jack, diamonds},
+		Card{queen, diamonds},
+		Card{king, diamonds},
+		Card{ace, diamonds},
+		Card{ten, diamonds},
+		Card{jack, diamonds},
+		Card{queen, spades},
+		Card{queen, spades},
+		Card{king, spades},
+		Card{ace, spades},
+		Card{ten, spades},
+		Card{jack, spades},
+	}
+	count := hand.Count()
+	if count[Card{jack, diamonds}] != 2 {
+		t.Errorf("There should be two jacks of diamonds")
+	}
+	if count[Card{queen, diamonds}] != 1 {
+		t.Errorf("There should be one jacks of diamonds")
+	}
+	if count[Card{king, diamonds}] != 1 {
+		t.Errorf("There should be one jacks of diamonds")
+	}
+	if count[Card{ace, diamonds}] != 1 {
+		t.Errorf("There should be one jacks of diamonds")
+	}
+	if count[Card{ten, diamonds}] != 1 {
+		t.Errorf("There should be one jacks of diamonds")
+	}
+	if count[Card{queen, spades}] != 2 {
+		t.Errorf("There should be two queen of spades")
+	}
+	if count[Card{king, spades}] != 1 {
+		t.Errorf("There should be one king of spades")
+	}
+	if count[Card{ace, spades}] != 1 {
+		t.Errorf("There should be one ace of spades")
+	}
+	if count[Card{ten, spades}] != 1 {
+		t.Errorf("There should be one ten of spades")
+	}
+	if count[Card{jack, spades}] != 1 {
+		t.Errorf("There should be one jack of spades")
+	}
+	if count[Card{jack, hearts}] != 0 {
+		t.Errorf("There should be zero jacks of hearts")
+	}
+}
+
+func TestMeld(t *testing.T) {
+	hands := []Hand{
+		Hand{
+			Card{jack, diamonds},
+			Card{queen, diamonds},
+			Card{king, diamonds},
+			Card{ace, diamonds},
+			Card{ten, diamonds},
+			Card{jack, diamonds},
+			Card{queen, spades},
+			Card{queen, spades},
+			Card{king, spades},
+			Card{ace, spades},
+			Card{ten, spades},
+			Card{jack, spades},
+		},
+		Hand{
+			Card{jack, diamonds},
+			Card{queen, diamonds},
+			Card{king, diamonds},
+			Card{queen, diamonds},
+			Card{king, diamonds},
+			Card{jack, hearts},
+			Card{jack, clubs},
+			Card{queen, spades},
+			Card{king, spades},
+			Card{ace, spades},
+			Card{ten, spades},
+			Card{jack, spades},
+		},
+		Hand{
+			Card{nine, diamonds},
+			Card{queen, diamonds},
+			Card{king, diamonds},
+			Card{ace, diamonds},
+			Card{ten, diamonds},
+			Card{jack, diamonds},
+			Card{queen, spades},
+			Card{queen, spades},
+			Card{king, spades},
+			Card{ace, spades},
+			Card{nine, spades},
+			Card{nine, spades},
+		},
+		Hand{
+			Card{jack, diamonds},
+			Card{queen, diamonds},
+			Card{king, diamonds},
+			Card{ace, diamonds},
+			Card{ten, diamonds},
+			Card{jack, diamonds},
+			Card{queen, diamonds},
+			Card{king, diamonds},
+			Card{ace, diamonds},
+			Card{ten, diamonds},
+			Card{ten, spades},
+			Card{jack, spades},
+		},
+	}
+	// spades, hearts, clubs, diamonds
+	results := [][]int{
+		[]int{47, 34, 34, 47},
+		[]int{27, 14, 14, 18},
+		[]int{12, 8, 8, 22},
+		[]int{4, 4, 4, 150},
+	}
+	for x := 0; x < len(hands); x++ {
+		for _, suit := range []int{spades, hearts, clubs, diamonds} {
+			var trump string
+			switch suit {
+			case hearts:
+				trump = "hearts"
+			case spades:
+				trump = "spades"
+			case diamonds:
+				trump = "diamonds"
+			case clubs:
+				trump = "clubs"
+			}
+			sort.Sort(hands[x])
+			if hands[x].Meld(suit) != results[x][suit] {
+				fmt.Printf("Testing hand #%d %v with %s\n", x, hands[x], trump)
+				_ = hands[x].MeldDebug(suit, true)
+				t.Errorf("Trump is %s, hand %d, %s, should be %d", trump, x, hands[x], results[x][suit])
+			}
+		}
+	}
 }
