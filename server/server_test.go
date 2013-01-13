bda6a2d9419d01c00fd88247306c9d0f4ddbedd2
Matthew Zimmerman <mzimmerman@gmail.com>
2013-01-13 01:03:27 -0500

    used the package prettytest on server_test.go

--- server/server_test.go
+++ server/server_test.go
@@ -3,15 +3,28 @@ package main
 
 import (
 	sdz "github.com/mzimmerman/sdzpinochle"
+	pt "github.com/remogatto/prettytest"
 	"sort"
 	"testing"
 )
+
+func TestFoo(t *testing.T) {
+	pt.RunWithFormatter(
+		t,
+		new(pt.TDDFormatter),
+		new(testSuite),
+	)
+}
 
 func C(c string) sdz.Card {
 	return sdz.Card(c)
 }
 
-func TestBidding(t *testing.T) {
+type testSuite struct {
+	pt.Suite
+}
+
+func (t *testSuite) TestBidding() {
 	// 9D QD TD TD AD JC QC KC 9H AH AH KS
 	hand := sdz.Hand{C("9D"), C("QD"), C("TD"), C("TD"), C("AD"), C("JC"), C("QC"), C("KC"), C("9H"), C("AH"), C("AH"), C("KS")}
 	sort.Sort(hand)
@@ -23,7 +36,5 @@ func TestBidding(t *testing.T) {
 		Playerid: ai.playerid,
 	}
 	action := <-ai.c
-	if 21 > action.Amount || action.Amount > 23 { // bid has 3 points of randomness
-		t.Errorf("Power bid should be between 21 and 23, not %d, for hand %s", action.Amount, hand)
-	}
+	t.False(21 > action.Amount || action.Amount > 23)
 }
